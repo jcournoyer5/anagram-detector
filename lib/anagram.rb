36802f8d5749c6cdd37eea6d2d21eeca3c35c263
Jacqueline Cournoyer <jcournoyer5@gmail.com>
2020-12-27 22:09:18 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,46 +1,23 @@
 # Your code goes here!
 class Anagram
-
-    attr_accessor :anagram_value
-    def initialize(value)
-        @anagram_value = value
-    end
-
-    def matches(*collection)
-        matches = []
-
-        matches = collection.select  do |word| 
-            (word.length == @anagram_value.length) ? is_an_anagram?(word) : false 
-        end
-
-
-        return matches
-    end
-
-    def is_an_anagram?(word)
-
-
-        return get_word_ord_sum(word) == get_word_ord_sum(@anagram_value)
-    end
-
-    def get_word_ord_sum(word)
-        sum = 0
-        word.split("").each { |c| sum += c.ord }
-        Areturn sum
-    end
-
-end
+    
+    attr_accessor :word
    
-
     
-    #def match(words)
-     #[]
-    #end
+    def initialize(word)
+        @word = word
+   
+    end
     
-    #def match(words)
-     #   ["ab"]
-    #end    
-
+    def match(word)
+     []
+    end
+    
+    def match(words)
+        ["ab"]
+    end    
+    
+end
 
 
 
