bf205ad130ced8b61b16c4a4db47b632b2cbbdef
Jacqueline Cournoyer <jcournoyer5@gmail.com>
2020-12-27 21:45:45 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,19 +1,36 @@
 # Your code goes here!
 class Anagram
+    
+    attr_accessor :anagram_value
+    def initialize(value)
+        @anagram_value = value
+    end
    
     
     def initialize(word)
         @word = word
+        
+      def matches(*collection)
+        matches = []
+
+        matches = collection.select  do |word| 
+            (word.length == @anagram_value.length) ? is_an_anagram?(word) : false 
+        end
+
+
+        return matches
+      end
+    end
    
-    end
+
     
-    def match(words)
-     []
-    end
+    #def match(words)
+     #[]
+    #end
     
-    def match(words)
-        ["ab"]
-    end    
+    #def match(words)
+     #   ["ab"]
+    #end    
     
 end
 
